29ba615f106b15eccbbf76f8192e8bfed50ee53f
jimmy-martin <jimmy.martin952@gmail.com>
2023-06-22 13:27:44 +0200

    update fake posts

--- src/data/index.ts
+++ src/data/index.ts
@@ -35,30 +35,30 @@ export const defaultEmptyBeat: BeatInterface = {
 
 export const posts = [
   {
-    title: 'Lorem',
+    title: 'Beatmaking pour les débutants',
     content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis metus ipsum. Donec libero eros, pulvinar et eleifend a, vulputate.',
+      "Plongez dans l'univers passionnant du beatmaking. Apprenez à créer vos propres rythmes, à manipuler des échantillons et à composer des mélodies accrocheuses.",
     imageUrl:
-      'https://images.unsplash.com/photo-1556155092-490a1ba16284?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-    date: 'Jan 4 2022',
+      'https://images.unsplash.com/photo-1563986768609-322da13575f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    date: '6 Janvier 2023',
     href: '#',
   },
   {
-    title: 'Lorem',
+    title: 'Les meilleures applications de beatmaking',
     content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis metus ipsum. Donec libero eros, pulvinar et eleifend a, vulputate.',
+      'Explorez notre sélection des meilleures applications de beatmaking pour produire de la musique où que vous soyez, que vous soyez sur iOS ou Android.',
     imageUrl:
-      'https://images.unsplash.com/photo-1620287341056-49a2f1ab2fdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-    date: 'Jan 4 2022',
+      'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    date: '22 Janvier 2023',
     href: '#',
   },
   {
-    title: 'Lorem',
+    title: 'Les principes de base du mixage',
     content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis metus ipsum. Donec libero eros, pulvinar et eleifend a, vulputate.',
+      "Le mixage est une étape cruciale de la production musicale. Découvrez comment équilibrer les niveaux, gérer l'espace stéréo et appliquer les effets pour un mixage parfait.",
     imageUrl:
-      'https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-    date: 'Jan 4 2022',
+      'https://images.unsplash.com/photo-1527443154391-507e9dc6c5cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    date: '8 Mars 2023',
     href: '#',
   },
 ]
